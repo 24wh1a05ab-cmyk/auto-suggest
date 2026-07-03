0944bcda8e48ce3a70cedc73ccf76d6e4795e23e
Archana <24wh1a05ab@bvrithyderabad.edu.in>
2026-07-03 13:41:40 +0530

    update port

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+
+const app = express()
+
+const PORT = process.env.PORT || 3002
+
+app.use(express.static("frontend"))
+
+app.listen(PORT, function(){
+    console.log("successfully running at http://localhost:"+PORT)
+})
+
+
+
